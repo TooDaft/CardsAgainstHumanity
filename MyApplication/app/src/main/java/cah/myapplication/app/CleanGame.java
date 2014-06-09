0544e453674818d5f4c02a836e102f55e486b082
cperrin10 <cperrin@live.com>
2014-06-09 16:10:02 -0400

    Added basic picker
    Added picker to the clean version with no additional function yet.

--- MyApplication/app/src/main/java/cah/myapplication/app/CleanGame.java
+++ MyApplication/app/src/main/java/cah/myapplication/app/CleanGame.java
@@ -1,17 +1,36 @@
 package cah.myapplication.app;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 
 
 public class CleanGame extends ActionBarActivity {
 
+    NumberPicker np;
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clean_game);
+
+        np = (NumberPicker) findViewById(R.id.numberPicker1);
+
+        np.setMinValue(4);
+        np.setMaxValue(15);
+        np.setWrapSelectorWheel(false);
+
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i2) {
+
+            }
+        });
     }
 
 
